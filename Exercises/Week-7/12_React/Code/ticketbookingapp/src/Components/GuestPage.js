bec7dae04f8b6633733aad713b014404fed07fd1
Tithi Ghosh <142734946+TithiGhosh18@users.noreply.github.com>
2025-07-31 19:54:34 +0530

    Delete Exercises/Week-7 directory

--- Exercises/Week-7/12_React/Code/ticketbookingapp/src/Components/GuestPage.js
+++
@@ -1,4 +0,0 @@
-function GuestPage() {
-  return <h2>Welcome Guest! Please login to book tickets.</h2>;
-}
-export default GuestPage;
--- Exercises/Week-7/12_React/Code/ticketbookingapp/src/Components/UserPage.js
+++
@@ -1,4 +0,0 @@
-function UserPage() {
-  return <h2>Welcome User! You can now book your flight tickets.</h2>;
-}
-export default UserPage;
